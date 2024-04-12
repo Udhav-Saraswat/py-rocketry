c69a7b40befe2afc7966a129e5776de106fa0674
Udhav-Saraswat <udhav.saraswat.17@gmail.com>
2024-04-12 15:17:19 +0530

    DEV : added sql scheduler

---
+++ rocketry-with-fastapi-master/sql-scheduler/main.py
@@ -0,0 +1,23 @@
+import asyncio
+from rocketry import Rocketry
+from rocketry.conds import every
+import logging
+
+app = Rocketry(config={"task_execution": "async"})
+
+@app.task(every('10 seconds', based="finish"))
+async def do_permanently():
+    "This runs for really long time"
+    print("Task-running")
+
+async def main():
+    sched = asyncio.create_task(app.serve())
+    await asyncio.wait([sched])
+
+if __name__ == "__main__":
+    # Print Rocketry's logs to terminal
+    logger = logging.getLogger("rocketry.task")
+    logger.addHandler(logging.StreamHandler())
+
+    # Run both applications
+    asyncio.run(main())
